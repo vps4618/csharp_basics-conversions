c731085f104e7680442217e97ca2827ccd18e7d5
vps4618 <vishwapraveen4618@gmail.com>
2022-08-06 18:51:39 +0530

    Add project files.

---
+++ conversions/default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="conversions._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="inputTextBox" runat="server" Height="25px" OnTextChanged="TextBox1_TextChanged" Width="302px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="ok" Width="135px" />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ conversions/default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace conversions
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            /*  int i;
+              i = Convert.ToInt32("Hello World!");
+             */
+            //   double k = 3.5;
+            //  int i = (int)k;
+            // resultLabel.Text = i.ToString();
+
+            string i = inputTextBox.Text;
+            int j = int.Parse(i);
+            int k = j + 1;
+            resultLabel.Text = k.ToString(); 
+        }
+    }
+}
